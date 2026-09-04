2ded74f3db1723d443aee7be9d83c05bdf385793
Tolga Bilgis <155099070+TolgaBilgis@users.noreply.github.com>
2026-09-04 19:26:09 -0400

    math: add geometric Vec3 operations

--- src/vec3.c
+++ src/vec3.c
@@ -14,3 +14,22 @@ Vec3 vec3_scale(Vec3 v, float scalar)
 {
     return (Vec3){v.x * scalar, v.y * scalar, v.z * scalar};
 }
+
+float vec3_dot(Vec3 a, Vec3 b)
+{
+    return a.x * b.x + a.y * b.y + a.z * b.z;
+}
+
+Vec3 vec3_cross(Vec3 a, Vec3 b)
+{
+    return (Vec3){
+        a.y * b.z - a.z * b.y,
+        a.z * b.x - a.x * b.z,
+        a.x * b.y - a.y * b.x
+    };
+}
+
+float vec3_length_squared(Vec3 v)
+{
+    return vec3_dot(v, v);
+}
--- src/vec3.h
+++ src/vec3.h
@@ -10,5 +10,8 @@ typedef struct {
 Vec3 vec3_add(Vec3 a, Vec3 b);
 Vec3 vec3_sub(Vec3 a, Vec3 b);
 Vec3 vec3_scale(Vec3 v, float scalar);
+float vec3_dot(Vec3 a, Vec3 b);
+Vec3 vec3_cross(Vec3 a, Vec3 b);
+float vec3_length_squared(Vec3 v);
 
 #endif
