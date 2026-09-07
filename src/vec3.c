f22f74343a6f9b4aaa8a46357b4eb6c54100eaf8
Tolga Bilgis <155099070+TolgaBilgis@users.noreply.github.com>
2026-09-07 19:14:27 -0400

    math: add Vec3 normalization

--- src/vec3.c
+++ src/vec3.c
@@ -1,5 +1,7 @@
 #include "vec3.h"
 
+#include <math.h>
+
 Vec3 vec3_add(Vec3 a, Vec3 b)
 {
     return (Vec3){a.x + b.x, a.y + b.y, a.z + b.z};
@@ -32,4 +34,20 @@ Vec3 vec3_cross(Vec3 a, Vec3 b)
 float vec3_length_squared(Vec3 v)
 {
     return vec3_dot(v, v);
+}
+
+float vec3_length(Vec3 v)
+{
+    return sqrtf(vec3_length_squared(v));
+}
+
+Vec3 vec3_normalize(Vec3 v)
+{
+    const float length = vec3_length(v);
+
+    if (length == 0.0f) {
+        return (Vec3){0.0f, 0.0f, 0.0f};
+    }
+
+    return vec3_scale(v, 1.0f / length);
 }
--- src/vec3.h
+++ src/vec3.h
@@ -13,5 +13,7 @@ Vec3 vec3_scale(Vec3 v, float scalar);
 float vec3_dot(Vec3 a, Vec3 b);
 Vec3 vec3_cross(Vec3 a, Vec3 b);
 float vec3_length_squared(Vec3 v);
+float vec3_length(Vec3 v);
+Vec3 vec3_normalize(Vec3 v);
 
 #endif
