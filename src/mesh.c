823e01c30e40f30b4af683838190b3b3657e6709
Tolga Bilgis <155099070+TolgaBilgis@users.noreply.github.com>
2026-09-12 19:34:24 -0400

    mesh: add indexed triangle faces

--- src/mesh.c
+++ src/mesh.c
@@ -17,11 +17,22 @@ static const MeshEdge CUBE_EDGES[] = {
     {0, 4}, {1, 5}, {2, 6}, {3, 7}
 };
 
+static const MeshTriangle CUBE_TRIANGLES[] = {
+    {0, 2, 1}, {0, 3, 2},
+    {4, 5, 6}, {4, 6, 7},
+    {0, 1, 5}, {0, 5, 4},
+    {3, 7, 6}, {3, 6, 2},
+    {0, 4, 7}, {0, 7, 3},
+    {1, 2, 6}, {1, 6, 5}
+};
+
 static const Mesh CUBE = {
     CUBE_VERTICES,
     sizeof(CUBE_VERTICES) / sizeof(CUBE_VERTICES[0]),
     CUBE_EDGES,
-    sizeof(CUBE_EDGES) / sizeof(CUBE_EDGES[0])
+    sizeof(CUBE_EDGES) / sizeof(CUBE_EDGES[0]),
+    CUBE_TRIANGLES,
+    sizeof(CUBE_TRIANGLES) / sizeof(CUBE_TRIANGLES[0])
 };
 
 const Mesh *mesh_cube(void)
--- src/mesh.h
+++ src/mesh.h
@@ -11,10 +11,18 @@ typedef struct {
 } MeshEdge;
 
 typedef struct {
+    unsigned int a;
+    unsigned int b;
+    unsigned int c;
+} MeshTriangle;
+
+typedef struct {
     const Vec3 *vertices;
     size_t vertex_count;
     const MeshEdge *edges;
     size_t edge_count;
+    const MeshTriangle *triangles;
+    size_t triangle_count;
 } Mesh;
 
 const Mesh *mesh_cube(void);
